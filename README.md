5b027ea83afec01058993dfa333d4dc9cf9ec195
pipeanayap <128739679+pipeanayap@users.noreply.github.com>
2024-09-20 01:15:51 +0000

    prioomer commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # marvel_project
 marvel_project
+este es un ejemplo de codespaces, te abre un visual studio code en linea 
---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('marvelForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por completar la encuesta sobre Marvel!');
+});
